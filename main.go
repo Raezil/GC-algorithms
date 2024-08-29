82ebc530a161f852f9933f78ea53fc31292e0e0a
Raezil <kamil.mazurek319@gmail.com>
2024-08-29 16:01:42 +0200

    fix

--- main.go
+++ main.go
@@ -1,53 +1,54 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-type Object struct{
-	id int
-	marked bool
+type Object struct {
+	id         int
+	marked     bool
 	references []*Object
-}
-
-func NewObject(id int) *Object{
-	return &Object{
-		id: id,
-		marked: false,
-		references: []*Object{},
-	}
 }
 
 var heap []*Object
 var roots []*Object
 
-func (o *Object) AddReference(obj *Object){
-	o.references = append(o.references, obj)
+func NewObject(id int) *Object {
+	return &Object{
+		id:         id,
+		marked:     false,
+		references: []*Object{},
+	}
 }
 
-func mark(obj *Object){
-	if obj == nil || obj.marked {
-		return
-	}
+func (o *Object) AddReference(obj *Object) {
+	o.references = append(o.references, obj)
+}
+func marking(obj *Object) {
 	obj.marked = true
 	for _, ref := range obj.references {
 		mark(ref)
 	}
 }
+func mark(obj *Object) {
+	if obj == nil || obj.marked {
+		return
+	}
+	marking(obj)
+}
 
-func sweep(){
-	for i:=0; i < len(heap); i++ {
+func sweep() {
+	for i := 0; i < len(heap); i++ {
 		obj := heap[i]
-		if !obj.marked{
+		if !obj.marked {
 			fmt.Printf("Object %d is unreachable and will be collected.\n", obj.id)
 			heap = append(heap[:i], heap[i+1:]...)
 			i--
-		}else{
+		} else {
 			obj.marked = false
 		}
 	}
 }
-
 
 func markSweep() {
 	for _, root := range roots {
